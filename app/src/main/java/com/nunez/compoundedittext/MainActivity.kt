23c0a29468e2f82d5d21b9ca61b749632447ee2d
Paul Nunez <paulnunezm@gmail.com>
2017-09-18 19:11:29 -0400

    added: a button to log all the values of the customFieldGroup.

--- app/src/main/java/com/nunez/compoundedittext/MainActivity.kt
+++ app/src/main/java/com/nunez/compoundedittext/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.nunez.compoundedittext
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showAllInputValues.setOnClickListener {
+            val list = addressField.getFieldValues()
+            Log.d("Main", "$list")
+        }
     }
 }
